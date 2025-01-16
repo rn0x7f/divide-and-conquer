980ed1b55a21dcfaa533143322cb5f4c8e1d59b9
rn0x7f <ampheriz0@gmail.com>
2025-01-15 19:28:42 -0600

    .

--- main.cpp
+++ main.cpp
@@ -1,21 +1,13 @@
- /**
- * SonarQube, open source software quality management tool.
- * Copyright (C) 2008-2013 SonarSource
- * http://github.com/SonarOpenCommunity/sonar-cxx
+/**
+ * @file main.cpp
+ * @brief Implementation of merge sort algorithm.
  *
- * SonarQube is free software; you can redistribute it and/or
- * modify it under the terms of the GNU Lesser General Public
- * License as published by the Free Software Foundation; either
- * version 3 of the License, or (at your option) any later version.
+ * @copyright Copyright (C) 2025
+ * @license This software is provided "as-is," without warranty of any kind.
+ *          Permission to use, copy, modify, and distribute this software for
+ *          any purpose is hereby granted without fee.
  *
- * SonarQube is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
- * Lesser General Public License for more details.
- *
- * You should have received a copy of the GNU Lesser General Public License
- * along with this program; if not, write to the Free Software Foundation,
- * Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ * @date 2025-01-15
  */
 #include <iostream>
 #include <vector>
